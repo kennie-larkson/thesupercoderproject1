10dfc7b1877195c72855ef6a31e719bdb876df55
LAWAL ABD'KEHINDE <32939546+kennie-larkson@users.noreply.github.com>
2020-05-08 16:32:41 +0100

    created GET and POST apis

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
 const express = require("express")
+const bodyparser = require("body-parser")
 const port = process.env.PORT || 3000
 
 const app = express()
+
+const postData = []
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.json())
+
+app.get("/",(req,res)=>{
+    res.send('{status: success}')
+       
+})
+
+app.post("/data",(req,res)=>{
+
+    const data = {
+        data: req.body.data
+    }
+
+    postData.push(data)
+
+    res.send(postData.map(data=> data))
+    postData.map(data=> console.log(data))
+    
+})
+
+app.get("/data",(req,res)=>{
+    res.sendFile( __dirname+'/index.html')
+   
+    
+})
 
 app.listen(port,()=>{
     console.log(`Application Server is running on port : ${port}`)
